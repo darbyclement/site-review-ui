0c918f92014bb8257cba7ef600a370e8c4229d71
tiffenychen <tc2963@columbia.edu>
2020-07-16 10:31:44 -0400

    Completed form and dashboard page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar.jsx"
 import Signin from "./components/Signin.jsx"
 import Signup from "./components/Signup.jsx"
+import Dashboard from "./components/Dashboard.jsx"
+
 
 import {
   BrowserRouter as Router,
@@ -30,7 +33,11 @@ function App() {
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
+        <NavBar />
         <Switch>
+        <Route path="/dashboard">
+            <Dashboard />
+          </Route>
           <Route path="/signup">
             <Signup />
           </Route>
